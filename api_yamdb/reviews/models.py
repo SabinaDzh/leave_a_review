ed57b79f3527617e9cf2b493afec5cc39cc2f482
Сабина Джавадова <SabinaIT@yandex.ru>
2024-06-11 21:24:49 +0300

    Modification of models, validate, serializers for Category, Genre, Title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+from .validators import validate_year
+
 
 class Category(models.Model):
+    """Модель категории произведения."""
     name = models.CharField(
         verbose_name='Название категории',
         max_length=256,
@@ -18,9 +21,11 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+        ordering = ['name']
 
 
 class Genre(models.Model):
+    """Модель жанра произведений."""
     name = models.CharField(
         verbose_name='Название жанра',
         max_length=256,
@@ -40,13 +45,14 @@ class Genre(models.Model):
 
 
 class Title(models.Model):
+    """Модель произведения."""
     name = models.CharField(
         verbose_name='Название произведения',
         max_length=256,
     )
     year = models.IntegerField(
         verbose_name='Год выпуска произведения',
-        # validators=(),
+        validators=[validate_year],
 
     )
     description = models.TextField(
@@ -72,3 +78,19 @@ class Title(models.Model):
     class Meta:
         verbose_name = 'Произведение'
         verbose_name_plural = 'Произведения'
+        ordering = ['name']
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+    )
+    title = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f'{self.genre} - {self.title}'
+
