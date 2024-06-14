c59c41ed48c4b424f7ceded30a1c46e8cbcf2092
Anna Sinitsyna <sinichka111@yandex.ru>
2024-06-14 21:19:31 +0300

    fix permissions

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -3,11 +3,8 @@ from rest_framework import permissions
 
 class IsAdminRole(permissions.BasePermission):
     """
-    Доступ к объекту только для админа.
+    Доступ к объектам только для админа.
     """
 
     def has_permission(self, request, view):
-        return request.user.is_admin
-
-    def has_object_permission(self, request, view, obj):
-        return request.user.is_admin
+        return (request.user.is_authenticated and request.user.is_admin)
