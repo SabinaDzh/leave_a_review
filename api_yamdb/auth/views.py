ffedadc5c80d1136adb0b80f1232e27d50b8f3c3
Anna Sinitsyna <sinichka111@yandex.ru>
2024-06-18 22:55:42 +0300

    Исправила view для регистрации пользователя

--- api_yamdb/auth/views.py
+++ api_yamdb/auth/views.py
@@ -4,22 +4,22 @@ from rest_framework import views
 from rest_framework.response import Response
 
 from auth.serializers import ConfirmationCodeSerializer, RegisterUserSerializer
-from auth.viewsets import CreateViewSet
 from users.models import User
 
 
-class RegisterUserViewSet(CreateViewSet):
+class RegisterUserView(views.APIView):
     """Регистрация пользователя"""
 
-    queryset = User
     serializer_class = RegisterUserSerializer
     permission_classes = (permissions.AllowAny,)
 
-    def create(self, request, *args, **kwargs):
-        data = super().create(request, *args, **kwargs)
-        if data.status_code == status.HTTP_201_CREATED:
-            data.status_code = status.HTTP_200_OK
-        return data
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.validated_data,
+                            status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ConfirmationCodeView(views.APIView):
@@ -27,7 +27,7 @@ class ConfirmationCodeView(views.APIView):
     serializer_class = ConfirmationCodeSerializer
     permission_classes = (permissions.AllowAny,)
 
-    def post(self, request: views.Request, *args, **kwargs):
+    def post(self, request):
 
         serializer = self.serializer_class(data=request.data)
 
