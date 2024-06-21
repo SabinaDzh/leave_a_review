109c617691ff4613fec4284b66164c58c1f19950
Dima_Novikov <dimanov13@gmail.com>
2024-06-21 19:04:36 +0300

    fix

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as UserAdminParentClass
 
 
 User = get_user_model()
 
 
-class UserAdmin(UserAdmin):
+class UserAdmin(UserAdminParentClass):
     list_display = (
         'username',
         'email',
